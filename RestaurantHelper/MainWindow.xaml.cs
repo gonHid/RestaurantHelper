5d08e279622b85ae2e650ae174b6a478a8c43d67
gonHid <gon.hidalgo.ch@gmail.com>
2024-05-17 21:56:40 -0400

    Botones dinamicos
    Se añade segmento para crear de forma dinamica los botones que representaran a las mesas disponibles en el restaurant

--- RestaurantHelper/MainWindow.xaml.cs
+++ RestaurantHelper/MainWindow.xaml.cs
@@ -20,7 +20,46 @@ namespace RestaurantHelper
         public MainWindow()
         {
             InitializeComponent();
+			List<Mesa> mesas = new List<Mesa>();
+			for (int i=0;i<=30;i++)
+			{
+				Mesa mesa = new Mesa();
+				mesa.Id = i;
+				mesas.Add(mesa);
+			}
+			GenerarMesas(mesas);
         }
 
-    }
+		private void GenerarMesas(List<Mesa> mesas)
+		{
+			foreach (var mesa in mesas)
+			{
+				// Crear un nuevo botón
+				Button button = new Button
+				{
+					Content = mesa.Id.ToString(),
+					Width = 400,
+					Height = 300,
+					Margin = new Thickness(10),
+					Name = "Mesa" + mesa.Id.ToString()
+				};
+				button.Click += Button_Click;
+				// Agregar el botón al contenedor
+				ContenedorMesas.Children.Add(button);
+			}
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			// Esta función se ejecuta cuando se haga clic en cualquier botón generado dinamicamente
+			var button = sender as Button;
+			if (button != null)
+			{
+				MessageBox.Show($"¡Has hecho clic en la mesa {button.Content}!");
+				// Aquí puedes agregar la lógica que desees realizar cuando se hace clic en un botón
+			}
+		}
+
+
+	}
 }
